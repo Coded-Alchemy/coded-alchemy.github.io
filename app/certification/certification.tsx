170252d6f6110d5e611661d29195e5aed1da7d41
Taji Abdullah <codedalchemy@gmail.com>
2024-12-28 18:52:15 -0500

    Added Certifications.

--- app/certification/certification.tsx
+++ app/certification/certification.tsx
@@ -1,21 +1,47 @@
 import {Footer, Header, Navigation} from "~/componets";
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 export function CertificationPage() {
     return (
-        <main className="flex items-center justify-center pt-16 pb-4">
+        <div className="flex items-center justify-center pt-16 pb-4">
             <div className="flex-1 flex flex-col items-center gap-16 min-h-0">
                 <Header/>
                 <Navigation />
-                <Content/>
+                <main>
+                    <Content/>
+                </main>
                 <Footer/>
             </div>
-        </main>
+        </div>
     );
 }
 
 function Content() {
+    const images = [
+        "app/certification/img/google_cs.png",
+        "app/certification/img/thm_aoc.png",
+        "app/certification/img/critical_thinking_problem_solving.png",
+        "app/certification/img/strategic_thinking.png",
+        "app/certification/img/how-to-be-more-strategic.png",
+        "app/certification/img/emotional_intelligence.png",
+        "app/certification/img/active_listening.png",
+        "app/certification/img/big_goals.png"
+    ];
+
     return (
-        <div>
+        <div className="max-w-[800px] w-full space-y-6 px-4">
+            <div className="rounded-3xl border border-gray-200 p-6 dark:border-gray-700 space-y-4">
+                <div className="box">
+                    <Carousel useKeyboardArrows={true}>
+                        {images.map((URL, index) => (
+                            <div className="slide">
+                                <img alt="sample_file" src={URL} key={index}/>
+                            </div>
+                        ))}
+                    </Carousel>
+                </div>
+            </div>
         </div>
     );
 }
--- app/routes.ts
+++ app/routes.ts
@@ -9,17 +9,4 @@ export default [
     route("about", "routes/about.tsx"),
     route("projects", "routes/projects.tsx"),
     route("certification", "routes/certification.tsx"),
-
-
-    // layout("certification", "routes/certification.tsx", [
-    //     index("certification/home.tsx"),
-    //     // route("contact", "./marketing/contact.tsx"),
-    // ])
-
-    // parent route
-    // route("dashboard", "./dashboard.tsx", [
-    //     // child routes
-    //     index("./home.tsx"),
-    //     route("settings", "./settings.tsx"),
-    // ]),
 ] satisfies RouteConfig;
