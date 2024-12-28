6a76985bd1e3dc72a11fdccd43f23c2d9fd667de
Taji Abdullah <codedalchemy@gmail.com>
2024-12-28 15:21:49 -0500

    Ok.....nah......

--- app/routes.ts
+++ app/routes.ts
@@ -1,8 +1,25 @@
-import {type RouteConfig, index, route} from "@react-router/dev/routes";
+import {
+    type RouteConfig,
+    index,
+    route,
+    layout} from "@react-router/dev/routes";
 
 export default [
     index("routes/home.tsx"),
     route("about", "routes/about.tsx"),
     route("projects", "routes/projects.tsx"),
-    route("certification", "routes/certification.tsx")
+    route("certification", "routes/certification.tsx"),
+
+
+    // layout("certification", "routes/certification.tsx", [
+    //     index("certification/home.tsx"),
+    //     // route("contact", "./marketing/contact.tsx"),
+    // ])
+
+    // parent route
+    // route("dashboard", "./dashboard.tsx", [
+    //     // child routes
+    //     index("./home.tsx"),
+    //     route("settings", "./settings.tsx"),
+    // ]),
 ] satisfies RouteConfig;
