c1bf872a00ac399f73397f980b813e3eb20d5428
Taji Abdullah <codedalchemy@gmail.com>
2024-12-28 18:59:49 -0500

    Added about info.

--- app/about/about.tsx
+++ app/about/about.tsx
@@ -29,14 +29,25 @@ function About() {
                     <div className="md:flex">
                         <div className="p-8">
                             <h1 className="text-3xl">About</h1>
-                            <p className="mt-2 text-white-500 mt-8">Since 2011 I have worked as a software engineer.
-                                I have worked on web applications, but mostly my focus has been on Android apps.
-                                During this time I have worked at:</p>
+                            <p className="mt-2 text-white-500 mt-8">
+                                As a Software Engineer with over a decade of experience, I leverage my extensive
+                                background in native Android development to deliver cutting-edge and user-friendly
+                                applications across diverse client domains. My proficiency spans the entire software
+                                development life cycle, encompassing planning, building, testing, deployment, and
+                                maintenance. Utilizing modern development practices and tools, I ensure the delivery of
+                                high-quality and efficient solutions.
+                            </p>
 
-                            <h3 className="text-2xl mt-8">Skills</h3>
-                            <p className="mt-2 text-white-500 mt-8">Since 2011 I have worked as a software engineer.
-                                I have worked on web applications, but mostly my focus has been on Android apps.
-                                During this time I have worked at:</p>
+                            <p className="mt-2 text-white-500 mt-8">
+                                With a keen passion for continuous learning, I actively explore and experiment with
+                                emerging technologies and frameworks. This commitment to staying abreast of industry
+                                trends allows me to remain at the forefront of innovation. I possess a strong
+                                foundation in key areas such as Java and Kotlin for object-oriented programming, Git
+                                for version control, SQL for database management, XML, JSON, and Android Studio with
+                                the Android SDK for mobile application development. The Model-View-ViewModel (MVVM)
+                                architecture serves as a cornerstone in my approach to crafting robust mobile
+                                applications.
+                            </p>
                         </div>
                     </div>
                 </div>
