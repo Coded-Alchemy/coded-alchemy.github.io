6d387bcb1d03b5d11b116049bc05fca277a35721
Taji Abdullah <codedalchemy@gmail.com>
2024-12-27 21:37:16 -0500

    Remove dead code.

--- app/home/home.tsx
+++ app/home/home.tsx
@@ -1,6 +1,5 @@
-import { useEffect } from "react";
-import { Footer, Header, Navigation } from "~/componets";
-import { useNavigate } from "react-router";
+import {useEffect} from "react";
+import {Footer, Header, Navigation} from "~/componets";
 
 function useTypewriterEffect(selector: string) {
     useEffect(() => {
@@ -45,46 +44,28 @@ export function HomePage() {
     return (
         <main className="flex items-center justify-center pt-16 pb-4">
             <div className="flex-1 flex flex-col items-center gap-16 min-h-0">
-                <Header />
-                <Navigation />
+                <Header/>
+                <Navigation/>
                 <div className="max-w-[800px] w-full space-y-6 px-4">
                     <div className="rounded-3xl border border-gray-200 p-6 dark:border-gray-700 space-y-4">
                         <div className="max-w-md mx-auto overflow-hidden md:max-w-2xl">
                             <div className="md:flex">
                                 <div className="md:shrink-0">
                                     <img
-                                        className="h-48 w-full object-cover md:h-full md:w-48"
+                                        className="h-64 w-full object-cover md:h-full md:w-64"
                                         src="/public/Designer.jpeg"
                                         alt="Taji Abdullah"
                                     />
                                 </div>
                                 <div className="p-8">
-                                    <h1 className="text-3xl typewriter text-center"></h1>
+                                    <h1 className="text-3xl typewriter text-center"/>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-                <Footer />
+                <Footer/>
             </div>
         </main>
-    );
-}
-
-function EnterButton() {
-    const navigate = useNavigate();
-
-    function handleClick() {
-        navigate("/about");
-    }
-
-    return (
-        <button
-            type="button"
-            onClick={handleClick}
-            className="bg-sky-500 hover:bg-sky-700"
-        >
-            Enter
-        </button>
     );
 }
