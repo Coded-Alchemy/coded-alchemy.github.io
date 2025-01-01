0b332c478952204a21221f313307ac582e43baa4
Taji Abdullah <codedalchemy@gmail.com>
2024-12-31 22:01:38 -0500

    did more stuff....

--- vite.config.ts
+++ vite.config.ts
@@ -7,8 +7,12 @@ import tsconfigPaths from "vite-tsconfig-paths";
 export default defineConfig({
   css: {
     postcss: {
-      plugins: [tailwindcss, autoprefixer],
+      plugins: [tailwindcss(), autoprefixer()],
     },
   },
   plugins: [reactRouter(), tsconfigPaths()],
+  build: {
+    outDir: "build/client", // Make sure this is the folder you want your build to go to
+  },
+  base: "/coded-alchemy.github.io/", // Set the base URL for GitHub Pages
 });
