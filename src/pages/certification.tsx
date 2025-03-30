c11ec4b9cbe8bff8298d3410f1276f1554b87cc4
Taji Abdullah <tajiabdullah1@gmail.com>
2025-03-30 10:57:41 -0400

    Added a cert. (#10)
    Co-authored-by: Taji Abdullah <codedalchemy@gmail.com>

--- src/pages/certification.tsx
+++ src/pages/certification.tsx
@@ -3,6 +3,7 @@ import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 export function CertificationPage() {
     const images = [
+        "/img/soc.png",
         "/img/google_cs.png",
         "/img/thm_pre_sec.png",
         "/img/thm_aoc.png",
@@ -19,6 +20,13 @@ export function CertificationPage() {
             <div className="rounded-3xl border border-gray-200 p-6 dark:border-gray-700 space-y-4">
                 <div className="box">
                     <h1 className="text-3xl">Certification</h1>
+                    <p className="mt-8">
+                        This is a collection of Certificates to showcase my dedication to continuous learning.
+                        Certificates range from soft skills to Cybersecurity.
+                    </p>
+                    <p className="mt-8">
+                        Click the sides of the certificates to progress forward and back.
+                    </p>
                     <Carousel useKeyboardArrows={true} className="mt-8">
                         {images.map((URL, index) => (
                             <div className="slide">
