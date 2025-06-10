3adc07540f6ae5581668b2be3e52d5631f039cb5
Taji Abdullah <codedalchemy@gmail.com>
2025-06-10 17:54:36 -0400

    Add new cert.

--- src/pages/certification.tsx
+++ src/pages/certification.tsx
@@ -3,6 +3,7 @@ import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 export function CertificationPage() {
     const images = [
+        "/img/security-plus.png",
         "/img/cc.png",
         "/img/soc.png",
         "/img/google_cs.png",
