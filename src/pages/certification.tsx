6cb35ce8059d949e336d78eee12eddc3eea8846b
Taji Abdullah <tajiabdullah1@gmail.com>
2025-06-10 18:23:24 -0400

    addCert (#20)
    * Add meta tag.
    
    * Add new cert.
    
    * Updated Certification page.
    
    ---------
    
    Co-authored-by: Taji Abdullah <codedalchemy@gmail.com>

--- src/pages/certification.tsx
+++ src/pages/certification.tsx
@@ -2,12 +2,16 @@ import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 export function CertificationPage() {
-    const images = [
+    const cyberCerts = [
+        "/img/security-plus.png",
         "/img/cc.png",
         "/img/soc.png",
         "/img/google_cs.png",
         "/img/thm_pre_sec.png",
-        "/img/thm_aoc.png",
+        "/img/thm_aoc.png"
+    ];
+
+    const softSkillCerts = [
         "img/critical_thinking_problem_solving.png",
         "/img/strategic_thinking.png",
         "/img/how-to-be-more-strategic.png",
@@ -26,10 +30,30 @@ export function CertificationPage() {
                         Certificates range from soft skills to Cybersecurity.
                     </p>
                     <p className="mt-8">
-                        Click the sides of the certificates to progress forward and back.
+                        Click the sides of the certificates to progress forward and back,
+                        or directly click a certificate underneath to jump directly to it.
                     </p>
+                </div>
+            </div>
+
+            <div className="rounded-3xl border border-gray-200 p-6 dark:border-gray-700 space-y-4 mt-8">
+                <div className="box">
+                    <h3 className="text-2xl">Cybersecurity</h3>
                     <Carousel useKeyboardArrows={true} className="mt-8">
-                        {images.map((URL, index) => (
+                        {cyberCerts.map((URL, index) => (
+                            <div className="slide">
+                                <img alt="Certification" src={URL} key={index}/>
+                            </div>
+                        ))}
+                    </Carousel>
+                </div>
+            </div>
+
+            <div className="rounded-3xl border border-gray-200 p-6 dark:border-gray-700 space-y-4 mt-8">
+                <div className="box">
+                    <h3 className="text-2xl">Soft Skills</h3>
+                    <Carousel useKeyboardArrows={true} className="mt-8">
+                        {softSkillCerts.map((URL, index) => (
                             <div className="slide">
                                 <img alt="Certification" src={URL} key={index}/>
                             </div>
